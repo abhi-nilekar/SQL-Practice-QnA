e7eb07f16f4ab20cf6e635657b078c967c8b1c30
abhi-nilekar <abhinilekar@gmail.com>
2023-07-28 19:44:46 +0530

    Create activation_rate.sql

---
+++ DataLemur/Signup Activation Rate [TikTok SQL Interview Question]/activation_rate.sql
@@ -0,0 +1,10 @@
+SELECT
+ ROUND(COUNT(texts.email_id)::DECIMAL/COUNT(DISTINCT emails.email_id),2) AS activation_rate
+FROM emails
+LEFT JOIN texts
+  ON emails.email_id = texts.email_id
+  AND texts.signup_action = 'Confirmed';
+
+--learnings
+--first, use of AND condition in joins
+--second, use of :: operator for type conversion
