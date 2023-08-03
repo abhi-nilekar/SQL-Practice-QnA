e632b16690573db18e175b8bf9f789c1a27ace4f
abhi-nilekar <abhinilekar@gmail.com>
2023-08-03 19:44:30 +0530

    Create card_launch.sql

---
+++ DataLemur/Card Launch Success [JPMorgan Chase SQL Interview Question]/card_launch.sql
@@ -0,0 +1,14 @@
+WITH cte_launch AS(
+SELECT 
+  card_name,
+  issued_amount,
+  MAKE_DATE(issue_year, issue_month,1) AS issue_date,
+  MIN(MAKE_DATE(issue_year, issue_month,1)) OVER(
+  PARTITION BY card_name
+  ) AS launch_date
+FROM monthly_cards_issued
+)
+SELECT card_name, issued_amount
+FROM cte_launch
+WHERE issue_date=launch_date
+ORDER BY issued_amount DESC;
