91199039ac8e8301de041d962454803f4133e370
abhi-nilekar <abhinilekar@gmail.com>
2023-10-03 18:58:28 +0530

    solved 1 problem from leetcode

---
+++ LeetCode/1211 Queries Quality and Percentage/query_quality_pct_myApproach.sql
@@ -0,0 +1,8 @@
+SELECT
+    query_name,
+    ROUND(SUM(rating/position)/COUNT(query_name),2) AS quality,
+    ROUND((SUM(CASE WHEN rating <3 THEN 1 ELSE 0 END)/COUNT(query_name))*100,2) AS poor_query_percentage 
+FROM Queries
+GROUP BY query_name
+
+-- my approach wroked
